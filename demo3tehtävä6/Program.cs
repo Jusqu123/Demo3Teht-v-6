a8b40e6758f01def7cd9d641e74e70a29c3e9ced
Jusqu123 <jusquu123@gmail.com>
2016-01-19 19:09:48 +0200

    kotitehtävä numero 6

---
+++ demo3tehtävä6/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarApplication
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         
+            Laite ps3 = new Laite();
+            ps3.Model = "PS3";
+            ps3.Color = "Piano Black";
+            ps3.Bits = 256;
+            ps3.backwardsCompatibility = false;
+            ps3.Price = 300;
+            ps3.PrintData();
+            ps3.PriceDrop(50);
+            Console.WriteLine("- Price after drop : ");
+            ps3.PrintData();
+       
+            Console.ReadLine();
+
+
+
+            Laite ps2 = new Laite("PS2");
+
+      
+            ps2.Price = 200;
+            ps2.Color = "black";
+            ps2.Bits = 128;
+            ps2.backwardsCompatibility = true;
+            ps2.PrintData();
+            ps2.PriceDrop(50);
+            Console.WriteLine("- Price after drop : ");
+            ps2.PrintData();
+        
+            Console.ReadLine();
+
+
+            Laite ps1 = new Laite("PS1");
+
+
+            ps1.Price = 200;
+            ps1.Color = "Grey";
+            ps1.Bits = 64;
+            ps1.backwardsCompatibility = false;
+            ps1.PrintData();
+            ps1.PriceDrop(50);
+            Console.WriteLine("- Price after drop : ");
+            ps1.PrintData();
+      
+            Console.ReadLine();
+
+
+        }
+    }
+}
+
+
+
+
+
+
+
+
---
+++ demo3tehtävä6/games.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarApplication
+{
+    class Laite
+    {
+        // fied member
+
+        //properties
+        public string Model { get; set; }
+        public string Color { get; set; }
+        public double Bits { get; set; }
+        public int Price { get; set; }
+        public bool backwardsCompatibility { get; set; }
+
+        // constructor
+        public Laite()
+        {
+            backwardsCompatibility = true;
+        }
+
+
+        // parametric constructor
+        public Laite(string model)
+        {
+            Model = model;
+        }
+
+        // method displays car data
+        public void PrintData()
+        {
+            Console.WriteLine("Console data: ");
+            Console.WriteLine("- model : " + Model);
+            Console.WriteLine("- Color : " + Color);
+            Console.WriteLine("- Bits : " + Bits);
+            Console.WriteLine("- Price : " + Price);
+            Console.WriteLine("- backwards Compatibility : " + backwardsCompatibility);
+      
+
+
+        }
+        public void PriceDrop(int NewPrice)
+        {
+            Price -= NewPrice;
+        }
+
+        public override string ToString()
+        {
+            return "Model :" + Model + "Color : " + Color + "IsOn : " + Bits + "Bits : "+ Price + "backwards Compatibility " + backwardsCompatibility;
+        }
+     
+    }
+
+}
